826a1cd314430e065ccea97b1bb432567478deaf
unknown <yanivprusman@gmail.com>
2024-06-14 21:14:05 +0300

    individual variables in the nvs ( instead of one json variable data)

--- main/gatt_demo.c
+++ main/gatt_demo.c
@@ -18,133 +18,145 @@ char *TAG = "BLE-Server tag ";
 uint8_t ble_addr_type;
 void ble_app_advertise(void);
 
-#define NVS_DATA_KEY "data"
-#define NVS_INITIALIZED_KEY "data"
-#define DEFAULT_VALUE "{\"initialized\":\"not initialized\",\"ssidName\":\"defaultSSID\",\"ssidPassword\":\"defaultPass\",\"myRemoteDeviceName\":\"MyRemoteDevice\"}"
-#define MAX_DATA_LENGTH 1000  
-
-char data[MAX_DATA_LENGTH + 1]; 
+#define NUM_VARIABLES (sizeof(variables) / sizeof(variables[0]))
 
 char* initializedFalseString = "not initialized";
 char* initializedTrueString = "initialized";
-char* initialized = NULL;
-char* ssidName = NULL;
-char* ssidPassword = NULL;
-char* myRemoteDeviceName = NULL;
 
 const uint8_t initializedCustomDataTrue[4]  = {0xDF, 0xAD, 0xBE, 0xEF};
 const uint8_t initializedCustomDataFalse[4] = {0xDF, 0xAD, 0xBE, 0xEE};
 
-void parseJson(){
-    esp_err_t err;
 
-    err = nvs_flash_init();
-    if (err != ESP_OK) {
-        ESP_LOGE(TAG, "Error initializing NVS: %s", esp_err_to_name(err));
+typedef struct {
+    char *name;
+    char *value;
+    const char *nvsKey;
+    const char *defaultValue;
+} variables_t;
+variables_t initialized = {"initialized",NULL,"var_1","not initialized"},
+    ssidName={"ssidName",NULL,"var_2",""},
+    ssidPassword={"ssidPassword",NULL,"var_3",""},
+    myRemoteDeviceName={"myRemoteDeviceName",NULL,"var_4","MyRemoteDevice"},
+    myRemoteDeviceID={"myRemoteDeviceID",NULL,"var_5",""};
+variables_t * variables[]={&initialized,&ssidName,&ssidPassword,&myRemoteDeviceName,&myRemoteDeviceID};
+
+void printVariables(){
+    for (int i = 0;i < NUM_VARIABLES;i++){
+        if((variables[i]->value)&&(variables[i]->name)){
+            printf("the value of %s is %s\n",variables[i]->name,variables[i]->value);
+        }
+    }
+}
+
+void fillVariablseFromJsonString(char* data){
+    cJSON *json = cJSON_Parse(data);
+    if (json == NULL) {
+        ESP_LOGE(TAG, "Error parsing JSON: %s", cJSON_GetErrorPtr());
         return;
     }
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    for (int i = 0;i < NUM_VARIABLES;i++){
+        cJSON *got = cJSON_GetObjectItemCaseSensitive(json,variables[i]->name);
+        if (cJSON_IsString(got) && (got->valuestring != NULL)) {
+            if (variables[i]->value) free(variables[i]->value);
+            variables[i]->value = strdup(got->valuestring);
+        }
+    }
+    cJSON_Delete(json);
+}
 
-    nvs_handle_t my_handle;
-    err = nvs_open("storage", NVS_READWRITE, &my_handle);
+void readVariablesFromNvs(){
+    esp_err_t err;
+
+    nvs_handle_t storage;
+    err = nvs_open("storage", NVS_READWRITE, &storage);
     if (err != ESP_OK) {
         ESP_LOGE(TAG, "Error opening NVS handle: %s", esp_err_to_name(err));
         return;
     }
+    for (size_t i = 0; i < NUM_VARIABLES; ++i) {
+        size_t required_size = 0;
+        err = nvs_get_str(storage, variables[i]->nvsKey, NULL, &required_size);
+        if (err != ESP_OK) {
+            ESP_LOGE(TAG, "Error getting size for %s: %s", variables[i]->name, esp_err_to_name(err));
+            continue;
+        }
 
-    size_t required_size = 0;
-    err = nvs_get_str(my_handle, "data", NULL, &required_size);
-    if (err != ESP_OK) {
-        ESP_LOGE(TAG, "Error getting data size: %s", esp_err_to_name(err));
-        nvs_close(my_handle);
-        return;
-    }
+        variables[i]->value = malloc(required_size);
+        if (variables[i]->value == NULL) {
+            ESP_LOGE(TAG, "Error allocating memory for %s", variables[i]->name);
+            continue;
+        }
 
-    char *data = malloc(required_size);
-    if (data == NULL) {
-        ESP_LOGE(TAG, "Error allocating memory for data");
-        nvs_close(my_handle);
-        return;
+        err = nvs_get_str(storage, variables[i]->nvsKey, variables[i]->value, &required_size);
+        if (err != ESP_OK) {
+            ESP_LOGE(TAG, "Error getting value for %s: %s", variables[i]->name, esp_err_to_name(err));
+            free(variables[i]->value);
+            variables[i]->value = NULL;
+            continue;
+        }
     }
-
-    err = nvs_get_str(my_handle, "data", data, &required_size);
-    if (err != ESP_OK) {
-        ESP_LOGE(TAG, "Error getting data: %s", esp_err_to_name(err));
-        free(data);
-        nvs_close(my_handle);
-        return;
+    nvs_close(storage);
+}
+void writeVariablesToNvs(){
+    nvs_handle_t storage;
+    nvs_open("storage", NVS_READWRITE, &storage);
+    for (size_t i = 0; i < NUM_VARIABLES; ++i) {
+        if (variables[i]->value != NULL){
+            nvs_set_str(storage, variables[i]->nvsKey, variables[i]->value);
+        };
     }
-
-    cJSON *json = cJSON_Parse(data);
-    if (json == NULL) {
-        ESP_LOGE(TAG, "Error parsing JSON: %s", cJSON_GetErrorPtr());
-        free(data);
-        nvs_close(my_handle);
-        return;
-    }
-
-    cJSON *cJsonInitialized = cJSON_GetObjectItemCaseSensitive(json, "initialized");
-    cJSON *cJsonSsidName = cJSON_GetObjectItemCaseSensitive(json, "ssidName");
-    cJSON *cJsonSsidPassword = cJSON_GetObjectItemCaseSensitive(json, "ssidPassword");
-    cJSON *cJsonMyRemoteDeviceName = cJSON_GetObjectItemCaseSensitive(json, "myRemoteDeviceName");
-
-    if (cJSON_IsString(cJsonInitialized) && (cJsonInitialized->valuestring != NULL)) {
-        if (initialized) free(initialized);
-        initialized = strdup(cJsonInitialized->valuestring);
-    }
-    if (cJSON_IsString(cJsonSsidName) && (cJsonSsidName->valuestring != NULL)) {
-        if (ssidName) free(ssidName);
-        ssidName = strdup(cJsonSsidName->valuestring);
-    }
-    if (cJSON_IsString(cJsonSsidPassword) && (cJsonSsidPassword->valuestring != NULL)) {
-        if (ssidPassword) free(ssidPassword);
-        ssidPassword = strdup(cJsonSsidPassword->valuestring);
-    }
-    if (cJSON_IsString(cJsonMyRemoteDeviceName) && (cJsonMyRemoteDeviceName->valuestring != NULL)) {
-        if (myRemoteDeviceName) free(myRemoteDeviceName);
-        myRemoteDeviceName = strdup(cJsonMyRemoteDeviceName->valuestring);
-    }
-
-    cJSON_Delete(json);
-    free(data);
-    nvs_close(my_handle);
-
+    nvs_commit(storage);
+    nvs_close(storage);
 }
 
 static int write_data(uint16_t conn_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt *ctxt, void *arg)
 {
     // Handle write operation
-    int len = ctxt->om->om_len;
-    if (len > MAX_DATA_LENGTH) {
-        // Truncate if the received data is longer than the maximum length
-        len = MAX_DATA_LENGTH;
-    }
+    // int len = ctxt->om->om_len;
+    // if (len > MAX_DATA_LENGTH) {
+    //     // Truncate if the received data is longer than the maximum length
+    //     len = MAX_DATA_LENGTH;
+    // }
     // Copy the data from the received buffer to the data variable
-    memcpy(data, ctxt->om->om_data, len);
+    // memcpy(data, ctxt->om->om_data, len);
     // Null-terminate the string
-    data[len] = '\0';
-    // Save the data to NVS
-    nvs_handle_t nvs_handle;
-    nvs_open("storage", NVS_READWRITE, &nvs_handle);
-    nvs_set_str(nvs_handle, NVS_DATA_KEY, data);
-    nvs_commit(nvs_handle);
-    nvs_close(nvs_handle);
-    printf("data from the client: %.*s\n", len, data);
+    // data[len] = '\0';  
+    fillVariablseFromJsonString((char *) ctxt->om->om_data);
+    writeVariablesToNvs();
+    // nvs_handle_t nvs_handle;
+    // nvs_open("storage", NVS_READWRITE, &nvs_handle);
+    // if (initialized != NULL){
+    //     nvs_set_str(nvs_handle, NVS_INITIALIZED, initialized);
+    // };
+    // if (ssidName != NULL){
+    //     nvs_set_str(nvs_handle, NVS_SSIDNAME, ssidName);
+    // };
+    // if (ssidPassword != NULL){
+    //     nvs_set_str(nvs_handle, NVS_SSIDPASSWORD, ssidPassword);
+    // };
+    // if (myRemoteDeviceName != NULL){
+    //     nvs_set_str(nvs_handle, NVS_MYREMOTEDEVICE_NAME, myRemoteDeviceName);
+    // };
+    // if (myRemoteDeviceID != NULL){
+    //     nvs_set_str(nvs_handle, NVS_MYREMOTEDEVICE_ID, myRemoteDeviceID);
+    // };
+    // nvs_commit(nvs_handle);
+    // nvs_close(nvs_handle);
+    // printf("data from the client: %.*s\n", len, data);
 
     return 0;
 }
 
 static int read_data(uint16_t conn_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt *ctxt, void *arg)
 {
-    parseJson();
-    ble_svc_gap_device_name_set(myRemoteDeviceName);
+    // ble_svc_gap_device_name_set(myRemoteDeviceName);
     cJSON *json_response = cJSON_CreateObject();
     if (json_response == NULL) {
         printf("Failed to create JSON object\n");
         return -1;
     }
-
-    cJSON_AddStringToObject(json_response, "myRemoteDeviceName", myRemoteDeviceName);
+    readVariablesFromNvs();
+    cJSON_AddStringToObject(json_response, "myRemoteDeviceName", myRemoteDeviceName.value);
 
     char *json_string = cJSON_PrintUnformatted(json_response);
     if (json_string == NULL) {
@@ -206,8 +218,8 @@ void ble_app_advertise(void)
     struct ble_hs_adv_fields fields;
     const char *device_name;
     memset(&fields, 0, sizeof(fields));
-    parseJson();
-    ble_svc_gap_device_name_set(myRemoteDeviceName);
+    readVariablesFromNvs();
+    ble_svc_gap_device_name_set(myRemoteDeviceName.value);
     device_name = ble_svc_gap_device_name();
     fields.name = (uint8_t *)device_name;
     fields.name_len = strlen(device_name);
@@ -216,9 +228,9 @@ void ble_app_advertise(void)
     uint16_t company_id = 0xFFFA;
     
     uint8_t custom_data[4];
-    if (strcmp(initialized, initializedTrueString) == 0){
+    if (strcmp(initialized.value, initializedTrueString) == 0){
         memcpy(custom_data, initializedCustomDataTrue, sizeof(initializedCustomDataTrue));
-    }else if (strcmp(initialized, initializedFalseString) == 0){
+    }else if (strcmp(initialized.value, initializedFalseString) == 0){
         memcpy(custom_data, initializedCustomDataFalse, sizeof(initializedCustomDataFalse));
     }
     uint8_t mfg_data[sizeof(company_id) + sizeof(custom_data)];
@@ -249,48 +261,57 @@ void host_task(void *param)
 }
 
 void app2();
-#define MAX_NAMESPACE_LEN 16
+
+void writeDefaultValues() {
+    esp_err_t err;
+    nvs_handle_t storage;
+    err = nvs_open("storage", NVS_READWRITE, &storage);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Error opening NVS handle: %s", esp_err_to_name(err));
+        return;
+    }
+
+    for (size_t i = 0; i < NUM_VARIABLES; ++i) {
+        err = nvs_set_str(storage, variables[i]->nvsKey, variables[i]->defaultValue);
+        if (err != ESP_OK) {
+            ESP_LOGE(TAG, "Error setting %s: %s", variables[i]->name, esp_err_to_name(err));
+        }
+    }
+
+    err = nvs_commit(storage);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Error committing NVS: %s", esp_err_to_name(err));
+    }
+
+    nvs_close(storage);
+}
+
+bool variablesAreEmpty(){
+    bool empty = false;
+    if (myRemoteDeviceID.value==NULL){
+        empty = true;
+        printf("variable myRemoteDeviceId is NULL\n");
+    }
+    return empty;
+}
+
+// void initialize(){
+//     for (size_t i = 0; i < NUM_VARIABLES; ++i) {
+//         variables[i]->value = NULL;
+//     }
+// }
+
 void app_main()
 {
     // app2();
     // return;
     nvs_flash_init();
-    nvs_handle_t nvs_handle;
-    size_t required_size;
-    nvs_open("storage", NVS_READWRITE, &nvs_handle);
-    
-    if (nvs_get_str(nvs_handle, NVS_DATA_KEY, NULL, &required_size) == ESP_ERR_NVS_NOT_FOUND) {
-        nvs_set_str(nvs_handle, NVS_DATA_KEY, DEFAULT_VALUE);
-        strcpy(data, DEFAULT_VALUE);
-    } else {
-        nvs_get_str(nvs_handle, NVS_DATA_KEY, data, &required_size);
+    readVariablesFromNvs();
+    if(variablesAreEmpty()){
+        writeDefaultValues();        
     }
-    nvs_commit(nvs_handle);
-    parseJson();    
-    nvs_close(nvs_handle);
-
     nimble_port_init();
-    // printf("B4 calling parseJson\n");
-    parseJson();    
-    // printf("after calling parseJson \n");
-    // if (ssidName!=NULL){
-    //     printf("ssidName: %s\n",ssidName);
-    // }
-    // if (myRemoteDeviceName!=NULL){
-    //     printf("myRemoteDeviceName: %s\n",myRemoteDeviceName);
-    // }
-    // if (initialized!=NULL){
-    //     printf("initialized: %s\n",initialized);
-    // }
-    // printf("end\n");
-    // return ;
-    // printf("myRemoteDeviceName: %s \n",myRemoteDeviceName);
-
-    // if (! initialized){
-    //     // write_default_value_to_nvs();
-    //     printf("myRemoteDevice initialized:%s\n",initialized);
-    // };
-    ble_svc_gap_device_name_set(myRemoteDeviceName);
+    // ble_svc_gap_device_name_set(myRemoteDeviceName);
     // ble_svc_gap_device_name_set("MyRemoteDevice1");
     ble_svc_gap_init();
     ble_svc_gatt_init();
@@ -300,43 +321,54 @@ void app_main()
     nimble_port_freertos_init(host_task);
 }
 
-void deleteDataFromNvs(void)
-{
-    nvs_handle_t nvs_handle;
-    esp_err_t err;
-    size_t required_size;
+// void deleteDataFromNvs(void)
+// {
+//     nvs_handle_t nvs_handle;
+//     esp_err_t err;
+//     size_t required_size;
 
-    // Initialize NVS
-    err = nvs_flash_init();
-    if (err != ESP_OK) {
-        printf("Error initializing NVS Flash: %s\n", esp_err_to_name(err));
-        return;
-    }
+//     // Initialize NVS
+//     err = nvs_flash_init();
+//     if (err != ESP_OK) {
+//         printf("Error initializing NVS Flash: %s\n", esp_err_to_name(err));
+//         return;
+//     }
 
-    // Open NVS
-    err = nvs_open("storage", NVS_READWRITE, &nvs_handle);
-    if (err != ESP_OK) {
-        printf("Error opening NVS: %s\n", esp_err_to_name(err));
-        return;
-    }
-    if (nvs_get_str(nvs_handle, NVS_DATA_KEY, NULL, &required_size) == ESP_OK) {
-        // Delete the NVS variable
-        err = nvs_erase_key(nvs_handle, NVS_DATA_KEY);
-        if (err != ESP_OK) {
-            printf("Error deleting NVS variable: %s\n", esp_err_to_name(err));
-        } else {
-            printf("NVS variable '%s' deleted successfully.\n", NVS_DATA_KEY);
-        }
-    }else if (nvs_get_str(nvs_handle, NVS_DATA_KEY, NULL, &required_size) == ESP_ERR_NVS_NOT_FOUND) {
-        printf("NVS variable '%s' not found.\n", NVS_DATA_KEY);
-    }
+//     // Open NVS
+//     err = nvs_open("storage", NVS_READWRITE, &nvs_handle);
+//     if (err != ESP_OK) {
+//         printf("Error opening NVS: %s\n", esp_err_to_name(err));
+//         return;
+//     }
+//     if (nvs_get_str(nvs_handle, NVS_DATA_KEY, NULL, &required_size) == ESP_OK) {
+//         // Delete the NVS variable
+//         err = nvs_erase_key(nvs_handle, NVS_DATA_KEY);
+//         if (err != ESP_OK) {
+//             printf("Error deleting NVS variable: %s\n", esp_err_to_name(err));
+//         } else {
+//             printf("NVS variable '%s' deleted successfully.\n", NVS_DATA_KEY);
+//         }
+//     }else if (nvs_get_str(nvs_handle, NVS_DATA_KEY, NULL, &required_size) == ESP_ERR_NVS_NOT_FOUND) {
+//         printf("NVS variable '%s' not found.\n", NVS_DATA_KEY);
+//     }
 
-    // Commit and close NVS
-    nvs_commit(nvs_handle);
-    nvs_close(nvs_handle);
-}
+//     // Commit and close NVS
+//     nvs_commit(nvs_handle);
+//     nvs_close(nvs_handle);
+// }
 
 void app2(void)
 {
-    deleteDataFromNvs();
+    printf("B4 initialize()\n");
+    // initialize();
+    nvs_flash_init();
+    printf("B4 read vriables from nvs\n");
+    readVariablesFromNvs();
+    printf("B4 check if variables are empty\n");
+    if(variablesAreEmpty()){
+        printf("variables are empty B4 writing default values\n");
+        writeDefaultValues();        
+    }
+    printf("printing values:\n");
+    printVariables();
 }
