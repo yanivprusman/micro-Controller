d7da0db68f509b52e04d8ec24c20e4cabd2602d9
unknown <yanivprusman@gmail.com>
2024-06-28 04:13:15 +0300

    read all partition table

--- main/myRemoteDevice.c
+++ main/myRemoteDevice.c
@@ -141,8 +141,6 @@ bool variablesAreEmpty(){
 void app_main()
 {
     esp_reset_reason_t r = esp_reset_reason();
-    // app2();
-    // return;
     printf("last reset reason: %d\n",r);
     esp_err_t err = nvs_flash_init();
     if(err !=ESP_OK){
@@ -156,6 +154,8 @@ void app_main()
     if(err !=ESP_OK){
         printf("error3: %s\n",esp_err_to_name(err));
     };
+    app2();
+    return;
     // wifi();
     example_connect(); 
     nimble();
@@ -170,7 +170,7 @@ void app_main()
     console();
     printf("ota \"https://esp.ya-niv.com/myRemoteDeviceBin\"\n");
 }
-
+int address = 0;
 #include <esp_partition.h>
 // #include "bootloader_support/include/bootloader_utility.h"
 int getNumberOfAppPartitions(){
@@ -190,26 +190,83 @@ int getNumberOfAppPartitions(){
     return ota_count;
 
 }
-int doFunction1(int argc, char **argv){
-    printf("new version 2\n");
-    if (argc != 3){
-        printf("usage: do 1 <uri>\n");
-        return -1;
+void print_partition_info() {
+    const esp_partition_t* partition;
+    esp_partition_iterator_t it = esp_partition_find(ESP_PARTITION_TYPE_APP, ESP_PARTITION_SUBTYPE_ANY, NULL);
+    
+    if (it != NULL) {
+        printf("App partitions:\n");
+        do {
+            partition = esp_partition_get(it);
+            printf("Name: %s\n", partition->label);
+            printf("Type: 0x%02x\n", partition->type);
+            printf("Subtype: 0x%02x\n", partition->subtype);
+            printf("Address: 0x%08lx\n", partition->address);
+            printf("Size: 0x%08lx (%ld KB)\n", partition->size, partition->size / 1024);
+            printf("Encrypted: %s\n", partition->encrypted ? "Yes" : "No");
+            printf("\n");
+        } while ((it = esp_partition_next(it)) != NULL);
+        esp_partition_iterator_release(it);
     }
-    int a = getNumberOfAppPartitions();
-    printf("partitions:%d\n",a);
+
+    it = esp_partition_find(ESP_PARTITION_TYPE_DATA, ESP_PARTITION_SUBTYPE_ANY, NULL);
+
+    if (it != NULL) {
+        printf("Data partitions:\n");
+        do {
+            partition = esp_partition_get(it);
+            printf("Name: %s\n", partition->label);
+            printf("Type: 0x%02x\n", partition->type);
+            printf("Subtype: 0x%02x\n", partition->subtype);
+            printf("Address: 0x%08lx\n", partition->address);
+            printf("Size: 0x%08lx (%ld KB)\n", partition->size, partition->size / 1024);
+            printf("Encrypted: %s\n", partition->encrypted ? "Yes" : "No");
+            printf("\n");
+        } while ((it = esp_partition_next(it)) != NULL);
+        esp_partition_iterator_release(it);
+    }
+}
+
+typedef void (*app_main_fn)(void);
+
+void call_ota_0() {
+    char * otaPartition = "ota_0";
+    const esp_partition_t *ota_partition = esp_partition_find_first(
+        ESP_PARTITION_TYPE_APP, ESP_PARTITION_SUBTYPE_APP_OTA_0, otaPartition);
+    
+    if (ota_partition == NULL) {
+        printf("Failed to find OTA partition\n");
+        return;
+    }
+    
+    printf("Found OTA partition at address 0x%08lx\n", ota_partition->address);
+    
+    // Cast the address to a function pointer and call it
+    app_main_fn ota_app_main = (app_main_fn) ota_partition->address+0x1000;
+    ota_app_main = (app_main_fn) address;
+    ota_app_main();
+}
+
+int doFunction1(int argc, char **argv){
+    // if (argc != 3){
+    //     printf("usage: do 1 <uri>\n");
+    //     return -1;
+    // }
+    // int a = getNumberOfAppPartitions();
+    // printf("partitions:%d\n",a);
+    print_partition_info();
     return 0;
 };
 int doFunction2(int argc, char **argv){
-    xTaskCreate(&ota_task, "asdf", 8192, NULL, 5, NULL);
-
-    // args_t args = {argc,argv};
-    // if (args.argc != 3){
-    //     printf("usage: do 2 <uri>\n");
-    //     return -1;
-    // }
-    
+    if (argc != 3){
+        printf("usage: do 2 <address>\n");
+        return -1;
+    }
+    address = atoi(argv[2]);
+    // xTaskCreate(&ota_task, "asdf", 8192, NULL, 5, NULL);
+    xTaskCreate((TaskFunction_t)call_ota_0, "call_ota_0", 8192, NULL, 5, NULL);    
     return 0;
 }
 void app2() {
+    console();
 }
