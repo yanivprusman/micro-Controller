2430d00ce48f193affbce61dbb0deaa990137e27
unknown <yanivprusman@gmail.com>
2024-07-01 04:43:46 +0300

    debug working

--- flashMonitor.ps1
+++ flashMonitor.ps1
@@ -2,5 +2,5 @@ $flash_args = Get-Content .\customBuild\flashArgs | Where-Object { $_ -notmatch 
 $flash_args_str = $flash_args -join "`n"
 $temp_flash_args_file = "customBuild\cleaned_flash_args"
 $flash_args_str | Out-File -FilePath $temp_flash_args_file -Encoding ASCII
-python -m esptool --chip esp32c3 -b 460800 --before default_reset --after hard_reset write_flash @$temp_flash_args_file
+python -m esptool --chip esp32s3 -b 460800 --before default_reset --after hard_reset write_flash @$temp_flash_args_file
 idf.py  monitor
